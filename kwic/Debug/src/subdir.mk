58ef106d2b1245d6325128558db3188edb7e5af4
Lukas Martinelli <me@lukasmartinelli.ch>
2014-11-09 19:49:36 +0100

    Renamed kwic.cpp to main.cpp

--- kwic/Debug/src/subdir.mk
+++ kwic/Debug/src/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/kwic.cpp 
+../src/main.cpp 
 
 OBJS += \
-./src/kwic.o 
+./src/main.o 
 
 CPP_DEPS += \
-./src/kwic.d 
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
